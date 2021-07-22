d0eb8a7648b15bf4d5e1e73fad88ae44ca7d8336
Ivonne Hernandez <ivonne702@gmail.com>
2021-07-22 16:05:30 -0600

    Add annotations to file

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -2,10 +2,16 @@
 // Use the double // to create a new comment
 
 //Build a Bear
+// Create a buildABear function with the following parameters: name, age, fur, clothes, specialPower
 function buildABear(name, age, fur, clothes, specialPower) {
+  //create a greeting variable and use interpolation to add the name parameter to your greeting
   var greeting = `Hey partner! My name is ${name} - will you be my friend?!`;
+  //create a demographics variable whose array contains name and age elements
   var demographics = [name, age];
+  //create a powerSaying variable and use concatenation to add the specialPower parameter to your saying
   var powerSaying = "Did you know that I can " + specialPower + " ?";
+  // create a builtBear object with the following key-value pairs:
+  // basicInfo = demographics, clothes = clothes, exterior = fur, cost = 49.99, sayings array with the following elements: greeting, powerSaying, "Goodnight my friend", isCuddly = true.
   var builtBear = {
     basicInfo: demographics,
     clothes: clothes,
@@ -14,21 +20,25 @@ function buildABear(name, age, fur, clothes, specialPower) {
     sayings: [greeting, powerSaying, "Goodnight my friend!"],
     isCuddly: true,
   };
-
+  // return the builtBear
   return builtBear
 }
+// call the buildABear function and pass it the following arguments
+console.log(buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares'));
 
-buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares');
-buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in');
+// call the buildABear function and pass it the following arguments
+console.log(buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in'));
 
 
 
 //FizzBuzz
+// create a fizzBuzz function with 3 parameters: num1, num2 and range
 function fizzBuzz(num1, num2, range) {
+  //create a for loop whose counter is less than or equal to the range
   for (var i = 0; i <= range; i++) {
     if (i % num1 === 0 && i % num2 === 0) {
       console.log('fizzbuzz');
-    } else if (i % num1 === 0) {
+    } else if (i % num1 === 0) { //otherwise, if
       console.log('fizz');
     } else if (i % num2 === 0) {
       console.log('buzz');
@@ -37,6 +47,12 @@ function fizzBuzz(num1, num2, range) {
     }
   }
 }
+//if i modulo num1 and i modulo num2 are strictly equal to 0, then console log 'fizzbuzz'
+//else if i modulo num1 is strictly equal to 0, then console log 'fizz'
+//else if i modulo num2 is strictly equal to 0, then console log 'buzz'
+//else console log the counter
 
-fizzBuzz(3, 5, 100);
-fizzbuzz(5, 8, 400);
+//call the fizzBuzz function with the following arguments
+fizzBuzz(3, 5, 10);
+//call the fizzBuzz function with the following arguments
+fizzBuzz(5, 8, 10);
